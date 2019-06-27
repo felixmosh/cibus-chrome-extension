dc39e48905f0b24476ba46b33abb3921808e8fca
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2019-06-27 22:15:25 +0300

    Disable future months

--- src/scripts/popup/components/MonthNavigation/MonthNavigation.tsx
+++ src/scripts/popup/components/MonthNavigation/MonthNavigation.tsx
@@ -1,16 +1,33 @@
 import { h } from 'preact';
 import * as styles from './MonthNavigation.scss';
+
 interface IMonthNavigationProps {
   currentDate: Date;
   onClickPrev: () => void;
   onClickNext: () => void;
 }
-export const MonthNavigation = ({ currentDate, onClickNext, onClickPrev }: IMonthNavigationProps) => {
+
+const currentMonth = new Date().getMonth();
+
+export const MonthNavigation = ({
+  currentDate,
+  onClickNext,
+  onClickPrev
+}: IMonthNavigationProps) => {
+  const isNextDisabled = currentDate.getMonth() >= currentMonth;
   return (
     <nav className={styles.monthNavigation}>
-      <button onClick={onClickPrev} className={styles.button}>❮</button>
+      <button onClick={onClickPrev} className={styles.button}>
+        ❮
+      </button>
       <span>{currentDate.toLocaleDateString('he-IL', { month: 'long' })}</span>
-      <button onClick={onClickNext} className={styles.button}>❯</button>
+      <button
+        onClick={onClickNext}
+        className={styles.button}
+        disabled={isNextDisabled}
+      >
+        ❯
+      </button>
     </nav>
   );
 };
